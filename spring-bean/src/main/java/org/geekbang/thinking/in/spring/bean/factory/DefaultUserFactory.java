9b3c7b84fc2dbfa27dbf93b1477c1262a138cde9
dowenliu-xyz <hawkdowen@hotmail.com>
2020-03-08 17:34:10 +0800

    bean initialization demo

--- spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/factory/DefaultUserFactory.java
+++ spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/factory/DefaultUserFactory.java
@@ -1,5 +1,9 @@
 package org.geekbang.thinking.in.spring.bean.factory;
 
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+
 /**
  * 默认 {@link UserFactory} 实现
  * <p>create at 2020/3/7</p>
@@ -7,5 +11,21 @@ package org.geekbang.thinking.in.spring.bean.factory;
  * @author liufl
  * @since 1.0
  */
-public class DefaultUserFactory implements UserFactory {
+public class DefaultUserFactory implements UserFactory, InitializingBean {
+
+
+    // 1. 基于 @PostConstruct 注解
+    @PostConstruct
+    public void init() {
+        System.out.println("@PostConstruct : UserFactory 初始化中...");
+    }
+
+    public void initUserFactory() {
+        System.out.println("自定义初始化方法 initUserFactory(): UserFactory 初始化中...");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("InitializingBean#afterPropertisSet : UserFactory 初始化中... ");
+    }
 }
