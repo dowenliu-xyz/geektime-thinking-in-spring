f9dd8ff5108fccf546f4a59729fe3127f60b6b43
dowenliu-xyz <hawkdowen@hotmail.com>
2020-03-09 17:36:30 +0800

    setter dependency injection demo

---
+++ dependency-injection/src/main/java/org/geekbang/thinking/in/spring/dependency/injection/ApiDependencySetterInjectionDemo.java
@@ -0,0 +1,63 @@
+package org.geekbang.thinking.in.spring.dependency.injection;
+
+import org.geekbang.thinking.in.spring.iov.overview.domain.User;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 基于 API 实现依赖 Setter 方法注入示例
+ * <p>create at 2020/3/9</p>
+ *
+ * @author liufl
+ * @since 1.0
+ */
+public class ApiDependencySetterInjectionDemo {
+    public static void main(String[] args) {
+        // 创建 BeanFactory 容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        // 生成 UserHolder 的 BeanDefinition
+        BeanDefinition userHolderBeanDefinition = createUserHolderBeanDefinition();
+        // 注册 UserHolder 的 BeanDefinition
+        applicationContext.registerBeanDefinition("userHolder", userHolderBeanDefinition);
+
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(applicationContext);
+        String xmlResourcePath = "classpath:/META-INF/dependency-lookup-context.xml";
+        // 加载 XML 资源，解析并生成 BeanDefinition
+        beanDefinitionReader.loadBeanDefinitions(xmlResourcePath);
+
+        // 启动应用上下文
+        applicationContext.refresh();
+
+        // 依赖查找并且创建 Bean
+        UserHolder userHolder = applicationContext.getBean(UserHolder.class);
+        System.out.println(userHolder);
+
+        // 关闭应用上下文
+        applicationContext.close();
+    }
+
+    /**
+     * 为 {@link UserHolder} 生成 {@link BeanDefinition}
+     */
+    private static BeanDefinition createUserHolderBeanDefinition() {
+        BeanDefinitionBuilder definitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(UserHolder.class);
+
+        definitionBuilder.addPropertyReference("user", "superUser");
+
+        return definitionBuilder.getBeanDefinition();
+    }
+
+//    @Bean
+//    public UserHolder userHolder(User user) {
+//        /*/
+//        return new UserHolder(user);
+//        /*/
+//        UserHolder userHolder = new UserHolder();
+//        userHolder.setUser(user);
+//        return userHolder;
+//        //*/
+//    }
+}
---
+++ dependency-injection/src/main/java/org/geekbang/thinking/in/spring/dependency/injection/UserHolder.java
@@ -0,0 +1,36 @@
+package org.geekbang.thinking.in.spring.dependency.injection;
+
+import org.geekbang.thinking.in.spring.iov.overview.domain.User;
+
+/**
+ * {@link User} 的 Holder 类
+ * <p>create at 2020/3/9</p>
+ *
+ * @author liufl
+ * @since 1.0
+ */
+public class UserHolder {
+    private User user;
+
+    public UserHolder() {
+    }
+
+    public UserHolder(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "UserHolder{" +
+                "user=" + user +
+                '}';
+    }
+}
