c0323c746da43d044b29da0ed0af041afa1b52b0
dowenliu-xyz <hawkdowen@hotmail.com>
2020-03-09 10:09:29 +0800

    dependency lookup type safety demo

---
+++ dependency-lookup/src/main/java/org/geekbang/thinking/in/spring/dependency/lookup/TypeSafetyDependencyLookupDemo.java
@@ -0,0 +1,72 @@
+package org.geekbang.thinking.in.spring.dependency.lookup;
+
+import org.geekbang.thinking.in.spring.iov.overview.domain.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * 类型安全 依赖查找示例
+ * <p>create at 2020/3/8</p>
+ *
+ * @author liufl
+ * @since 1.0
+ */
+public class TypeSafetyDependencyLookupDemo {
+    public static void main(String[] args) {
+        // 创建 BeanFactory 容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        // 将当前类作为配置类（Configuration Class）
+        applicationContext.register(TypeSafetyDependencyLookupDemo.class);
+        // 启动应用上下文
+        applicationContext.refresh();
+        // 演示 BeanFactory#getBean 安全性
+        displayBeanFactoryGetBean(applicationContext);
+        // 演示 ObjectFactory#getObject 方法安全性
+        displayObjectFactoryGetObject(applicationContext);
+        // 演示 ObjectProvider#getIfAvailable 方法安全性
+        displayObjectProviderIfAvailable(applicationContext);
+        // 演示 ListableBeanFactory#getBeansOfType 方法安全性
+        displayListableBeanFactory(applicationContext);
+        // 演示 ObjectProvider Stream 操作安全性
+        displayObjectProviderStreamOps(applicationContext);
+        // 关闭应用上下文
+        applicationContext.close();
+    }
+
+    private static void displayObjectProviderStreamOps(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
+        printBeansException("displayObjectProviderStreamOps", () -> userObjectProvider.forEach(System.out::println));
+    }
+
+    private static void displayListableBeanFactory(ListableBeanFactory beanFactory) {
+        printBeansException("displayListableBeanFactory", () -> beanFactory.getBeansOfType(User.class));
+    }
+
+    private static void displayObjectProviderIfAvailable(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
+        printBeansException("displayObjectProviderIfAvailable", userObjectProvider::getIfAvailable);
+    }
+
+    private static void displayObjectFactoryGetObject(AnnotationConfigApplicationContext applicationContext) {
+        // ObjectProvider is ObjectFactory
+        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
+        printBeansException("displayObjectFactoryGetObject", userObjectProvider::getObject);
+    }
+
+    public static void displayBeanFactoryGetBean(BeanFactory beanFactory) {
+        printBeansException("displayBeanFactoryGetBean", () -> beanFactory.getBean(User.class));
+    }
+
+    private static void printBeansException(String source, Runnable runnable) {
+        System.err.println("---");
+        System.err.println("Source from :" + source);
+        try {
+            runnable.run();
+        } catch (BeansException e) {
+            e.printStackTrace();
+        }
+    }
+}
