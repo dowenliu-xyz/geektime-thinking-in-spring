7bab97ddb6b41610b8a8459fd50fd9445dfece8f
dowenliu-xyz <hawkdowen@hotmail.com>
2020-03-13 13:37:49 +0800

    AnnotatedBeanDefinitionParsingDemo

---
+++ bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/AnnotatedBeanDefinitionParsingDemo.java
@@ -0,0 +1,29 @@
+package org.geekbang.thinking.in.spring.bean.lifecycle;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
+
+/**
+ * 注解 BeanDefinition 解析示例
+ * <p>create at 2020/3/13</p>
+ *
+ * @author liufl
+ * @since 1.0
+ */
+public class AnnotatedBeanDefinitionParsingDemo {
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        // 基于 Java 注解的 BeanDefinitionReader 实现
+        AnnotatedBeanDefinitionReader beanDefinitionReader = new AnnotatedBeanDefinitionReader(beanFactory);
+        int beanDefinitionCountBefore = beanFactory.getBeanDefinitionCount();
+        // 注册当前类（非@Component Class）
+        beanDefinitionReader.register(AnnotatedBeanDefinitionParsingDemo.class);
+        int beanDefinitionCountAfter = beanFactory.getBeanDefinitionCount();
+        int beanDefinitionCount = beanDefinitionCountAfter - beanDefinitionCountBefore;
+        System.out.println("已加载 BeanDefinition 数量：" + beanDefinitionCount);
+        // 普通的 Class 作为 Component 注册到 Spring IoC 容器后，通常 Bean 名称为 短类名首字母小写
+        // Bean 名称生成来自于 BeanNameGenerator, 注解实现 AnnotationBeanNameGenerator
+        AnnotatedBeanDefinitionParsingDemo demo = beanFactory.getBean("annotatedBeanDefinitionParsingDemo", AnnotatedBeanDefinitionParsingDemo.class);
+        System.out.println(demo);
+    }
+}
